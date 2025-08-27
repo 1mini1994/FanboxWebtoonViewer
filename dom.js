d8a0a16f057433b950e069adbb2ebacb86d913c2
1mini <xnfmzmwhr@naver.com>
2025-08-27 20:37:37 +0900

    코드 정리

--- dom.js
+++ dom.js
@@ -1,16 +1,23 @@
-let cursorTimeout;
+// let cursorTimeout;
 
 run();
 
 function run() {
+  const imageContainerKey = ".TwoColumnLayout__Wrapper-sc-rd992x-0.hHQIGr";
+  const imageShirinkContainerKey = "hHQIGr";
+  const navKey = ".Header__Wrapper-sc-60utt6-0.cplgnZ";
+  const backgroundKey =
+    "[class='styled__FullWidthWrapper-sc-1vjtieq-1 ccHLHk']";
   document
-    .querySelector(".TwoColumnLayout__Wrapper-sc-rd992x-0.hHQIGr")
-    .classList.remove("hHQIGr"); // 이미지를 축소시키는 식별자 삭제 시키는 코드
-  document.querySelector(".Header__Wrapper-sc-60utt6-0.cplgnZ").style.display =
-    "none"; // 화면 최상단 네비게이션 삭제 시키는 코드
-  document.querySelector(
-    "[class='styled__FullWidthWrapper-sc-1vjtieq-1 ccHLHk']"
-  ).style.backgroundColor = "#f5f5f5"; // 이미지 영역 뒤에 여백 배경색을 pixiv vertical viewer와 동일한 색상 값으로 변경하는 코드
+    .querySelector(imageContainerKey)
+    ?.classList?.remove(imageShirinkContainerKey); // 이미지를 축소시키는 식별자 삭제 시키는 코드
+  if (document.querySelector(navKey).style != null) {
+    document.querySelector(navKey).style.display = "none"; // 화면 최상단 네비게이션 삭제 시키는 코드
+  }
+
+  if (document.querySelector(backgroundKey) != null) {
+    document.querySelector(backgroundKey).style.backgroundColor = "#f5f5f5"; // 이미지 영역 뒤에 여백 배경색을 pixiv vertical viewer와 동일한 색상 값으로 변경하는 코드
+  }
 
   const elem = document.documentElement; // 전체 문서 또는 특정 요소
   if (elem.requestFullscreen) {
@@ -38,15 +45,15 @@ function run() {
   // setAutoHideCursor();
 }
 
-function setAutoHideCursor() {
-  document.addEventListener("mousemove", () => {
-    // 마우스가 움직이면 기존에 설정된 타이머를 클리어합니다.
-    clearTimeout(cursorTimeout);
+// function setAutoHideCursor() {
+//   document.addEventListener("mousemove", () => {
+//     // 마우스가 움직이면 기존에 설정된 타이머를 클리어합니다.
+//     clearTimeout(cursorTimeout);
 
-    // 3초 후에 마우스 커서를 숨깁니다.
-    document.body.style.cursor = "default"; // 이 줄을 사용하면 기본 커서로 변경됩니다. 'none'을 사용하면 숨깁니다.
-    cursorTimeout = setTimeout(() => {
-      document.body.style.cursor = "none";
-    }, 3000);
-  });
-}
+//     // 3초 후에 마우스 커서를 숨깁니다.
+//     document.body.style.cursor = "default"; // 이 줄을 사용하면 기본 커서로 변경됩니다. 'none'을 사용하면 숨깁니다.
+//     cursorTimeout = setTimeout(() => {
+//       document.body.style.cursor = "none";
+//     }, 3000);
+//   });
+// }
